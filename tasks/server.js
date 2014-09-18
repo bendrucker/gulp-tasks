0405002d9a2e2003e4a46e8606726905a75f0bb2
Ben Drucker <bvdrucker@gmail.com>
2014-09-18 12:13:22 -0400

    Create a task for starting a superstatic server

---
+++ tasks/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*
+Starts a superstatic server
+*/
+
+var _           = require('lodash');
+var Promise     = require('bluebird');
+var superstatic = require('superstatic');
+var livereload  = require('connect-livereload');
+var util        = require('gulp-util');
+var env         = require('../utils/env');
+
+module.exports = function (options) {
+  var server = superstatic(_.defaults(options || {}, {
+    host: '0.0.0.0',
+    port: 8000
+  }));
+
+  if (env.is('development')) server.use(livereload());
+
+  return Promise.promisify(server.listen, server)();
+};
