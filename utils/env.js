ad785d2b7492a6e9df43ec894cf93c332cafb2b9
Ben Drucker <bvdrucker@gmail.com>
2014-09-18 08:17:43 -0400

    Environment utility

---
+++ utils/env.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var util = require('gulp-util');
+var iff  = require('gulp-if');
+
+exports.env = util.env.env || 'development';
+
+exports.is = function (environments) {
+  environments = Array.isArray(environments) ? environments : [].slice.call(arguments, 0);
+  return environments.indexOf(exports.env) !== -1;
+};
+
+exports.isDev = exports.is('development');
+
+exports.if = function (environments) {
+  return iff.apply(null, [exports.is(environments)].concat([].slice.call(arguments, 1)));
+};
+exports.not = function (environments) {
+  return iff.apply(null, [!exports.is(environments)].concat([].slice.call(arguments, 1)));
+};
