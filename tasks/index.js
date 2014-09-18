b104142a08041589b4c08e7535f952efe40043c4
Ben Drucker <bvdrucker@gmail.com>
2014-09-18 14:01:15 -0400

    syntax error

--- tasks/index.js
+++ tasks/index.js
@@ -10,7 +10,7 @@ minify index.html and replace revved assets
 
 module.exports = function (stream) {
   return stream
-    .pipe(env.not('development', manifest.replace()));
+    .pipe(env.not('development', manifest.replace()))
     .pipe(env.not('development', htmlmin({
       collapseWhitespace: true
     })));
