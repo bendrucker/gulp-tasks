09b7d2d3ef75e2fea5f6fb8c913969df90d64442
Ben Drucker <bvdrucker@gmail.com>
2014-09-23 18:42:40 -0400

    Finalize bundle ops for watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -7,15 +7,25 @@ var bundle = require('./bundle');
 module.exports = function (srcMap, options) {
   for (var path in srcMap) {
     var task = srcMap[path];
-    if (task !== 'bundle') gulp.watch(path, [srcMap[path]]);
+    if (task !== 'bundle') {
+      gulp.watch(path, [srcMap[path]]);
+    }
+    else {
+      bundle.get(path, {
+        watch: true
+      })
+      .on('update', writeBundle);
+    }
+  }
+
+  function writeBundle () {
+    bundle.bundle().pipe(gulp.dest(options.build));
   }
 
   server.livereload.listen();
   gulp.watch(options.glob, server.livereload.changed);
 
-  bundle.get().on('update', function () {
-    bundle.bundle().pipe(gulp.dest(options.build));
-  });
+  return writeBundle();  
 };
 
 module.exports.rawSrc = true;
