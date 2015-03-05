1ce9d49488b873f2857f9d55ae66e095702cd17e
Ben Drucker <bvdrucker@gmail.com>
2015-03-05 09:12:29 -0500

    Don't append template caching script for production builds
    Instead I'm importing templates directly to the bundle using partialify

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -1,15 +1,11 @@
 'use strict';
 
-var gulp        = require('gulp');
 var browserify  = require('browserify');
 var watchify    = require('watchify');
 var source      = require('vinyl-source-stream');
 var buffer      = require('vinyl-buffer');
-var streamqueue = require('streamqueue');
 var uglify      = require('gulp-uglify');
 var rev         = require('gulp-rev');
-var concat      = require('gulp-concat');
-var templates   = require('./templates');
 var env         = require('../utils/env');
 var manifest    = require('../utils/manifest');
 
@@ -17,8 +13,7 @@ var b;
 function bro (path, options) {
   if (!b) {
     b = browserify(options.watch && watchify.args)
-      .add(path)
-      .transform('browserify-shim');
+      .add(path);
 
     if (options.watch) watchify(b);
     if (!env.isDev) b.transform(require('uglifyify'));
@@ -39,13 +34,10 @@ module.exports = function (path, options) {
     return appBundle;
   }
   else {
-    return streamqueue({objectMode: true}, appBundle, templates.cache(gulp.src(options.templates), {
-      module: options.module
-    }))
-    .pipe(concat('app.js'))
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(manifest());
+    return appBundle
+      .pipe(uglify())
+      .pipe(rev())
+      .pipe(manifest());
   }
 };
 
